8d0bc8c63911a5451f7da66ed125d179b2a627ef
someMinecrafter <67809388+someMinecrafter@users.noreply.github.com>
2021-07-09 07:57:44 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,4 +6,16 @@ local function printkv(t)
 	end
 end
 
-printkv(pokedex)
+local hash = {}
+local function printkvRecursively(t, depth)
+	depth = depth or 0
+	for k,v in pairs(t) do
+		if type(v) == "table" and not hash[v] then
+			hash[v] = true
+			printkvRecursively(v, depth + 1)
+		end
+		print(("%sKey: %s, Value: %s"):format(("	"):rep(depth), k, v))
+	end
+end
+
+printkvRecursively(pokedex)
